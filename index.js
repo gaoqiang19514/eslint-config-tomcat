9145d94139127331663aa1c89f86d64962b8e7b2
高强 <gaoqiang@hua-cloud.com.cn>
2021-04-02 14:42:54 +0800

    refactor: 撤销放宽的限制

--- index.js
+++ index.js
@@ -40,49 +40,5 @@ module.exports = {
         }],
     },
     rules: {
-        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-        'react/forbid-prop-types': ['off'],
-        'react/require-default-props': ['off'],
-        'jsx-a11y/no-static-element-interactions': ['off'],
-        'jsx-a11y/click-events-have-key-events': ['off'],
-        'jsx-a11y/mouse-events-have-key-events': ['off'],
-        'jsx-a11y/label-has-associated-control': ['off'],
-        'jsx-a11y/no-noninteractive-element-interactions': ['off'],
-
-        // 匿名组件设置display-name
-        'react/display-name': ['off'],
-
-        // 允许省略图片的alt属性
-        'jsx-a11y/alt-text': ['off'],
-
-        // 允许在constructor外声明state
-        'react/state-in-constructor': ['off'],
-
-        // 允许在组件内声明静态属性
-        'react/static-property-placement': ['off'],
-
-        'global-require': ['off'],
-
-        // 禁止使用变量解构 关闭
-        'react/jsx-props-no-spreading': ['off'],
-
-        // 禁止使用上下文存在的变量名 关闭
-        'no-shadow': ['off'],
-
-        // 禁止使用style['container'] 关闭
-        'dot-notation': ['off'],
-
-        // react.component结构顺序
-        'react/sort-comp': [
-            'error',
-            {
-                order: [
-                    'static-methods',
-                    'lifecycle',
-                    'render',
-                    'everything-else',
-                ],
-            },
-        ],
     },
 };
