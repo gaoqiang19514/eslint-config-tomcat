cded5baf30f3c79a21603706aad836e75add8073
高强 <gaoqiang@hua-cloud.com.cn>
2021-04-02 15:13:10 +0800

    chore: 格式化代码

--- index.js
+++ index.js
@@ -1,45 +1,48 @@
 const path = require('path');
 
 module.exports = {
-    parser: 'babel-eslint',
-    extends: [
-        'airbnb',
-        'plugin:prettier/recommended',
-        'plugin:react/recommended',
-        'plugin:react-hooks/recommended',
-    ],
-    env: {
-        jest: true,
-        browser: true,
-        node: true,
-        es6: true,
+  parser: 'babel-eslint',
+  extends: [
+    'airbnb',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  env: {
+    jest: true,
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  globals: {
+    wx: true,
+    qq: true,
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2021,
+    ecmaFeatures: {
+      jsx: true,
     },
-    globals: {
-        wx: true,
-        qq: true,
-    },
-    parserOptions: {
-        sourceType: 'module',
-        ecmaVersion: 2021,
-        ecmaFeatures: {
-            jsx: true,
-        },
-    },
-    settings: {
-        'import/resolver': ['node', {
-            webpack: {
-                config: {
-                    resolve: {
-                        alias: {
-                            '@': path.resolve('src'),
-                        },
-                        extensions: ['.js', '.jsx'],
-                    },
-                },
+  },
+  settings: {
+    'import/resolver': [
+      'node',
+      {
+        webpack: {
+          config: {
+            resolve: {
+              alias: {
+                '@': path.resolve('src'),
+              },
+              extensions: ['.js', '.jsx'],
             },
-        }],
-    },
-    "rules": {
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-      }
+          },
+        },
+      },
+    ],
+  },
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
 };
